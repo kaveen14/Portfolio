b03d02c42b094ed0ed675d3d6c29fe1ecef0d73d
ksite1 <kaveenkumarc@gmail.com>
2026-07-20 05:57:28 +0530

    Deployment Process

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Portfolio/',
   plugins: [react()],
+  server: {
+    port: 5175,
+    strictPort: true,
+    host: true,
+  },
 })
